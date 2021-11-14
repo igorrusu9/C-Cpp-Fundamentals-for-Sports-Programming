63b9afed214fc7effbe13f6de10bee4b5e631fb1
igorrusu9 <65688712+igorrusu9@users.noreply.github.com>
2021-11-14 09:36:17 -0500

    1-Переменные

---
+++ 1-Переменные/programs-1.1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+/* programm_1.1-1
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0; */
+
+/* programm_1.1-2
+int a, b;
+cin >> a >> b;
+cout << 2*(a + b) << endl;
+return 0;*/
+
+/* programm_1.1-3
+int a;
+cin >> a;
+// cout << pow (a,3) << endl;
+cout << a*a*a << endl;
+return 0;*/
+
+/* programm_1.1-4
+int a;
+cin >> a;
+cout << a%7 << endl;
+return 0;*/
+
+/* programm_1.1-5
+    int a;
+    cin >> a;
+    cout << a / 1000 << (a / 10) % 10 << (a / 100) % 10 << (a % 100) % 10;
+   // cout << "\n---------------------------";
+   // cout << "\na1=" << (a / 1000);
+   //cout << "\na2=" << ((a / 10) % 10);
+   //cout << "\na3=" << ((a / 100) % 10);
+   // cout << "\na4=" << ((a % 100) % 10);
+    return 0;*/
+
+/* programm_1.1-6
+int a, b;
+cin >> a >> b;
+cout << (a / b) + 1 - a / (a + a % b) << endl;
+return 0; */
+
+/* programm_1.1-6
+int a;
+cin >> a;
+cout << a * a << endl;
+return 0; */
+
+}
---
+++ 1-Переменные/programs-1.2.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+/*program_1.2-1
+    long long a, b;
+    cin >> a >> b;
+    cout << a * b << endl;
+    return 0;*/
+
+    /*program_1.2-2
+    unsigned long long a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;*/
+
+    /*program_1.2-3
+    long long a, b;
+    cin >> a >> b;
+    cout << (a % b + b) % b << endl;
+    return 0;*/
+
+    /*program_1.2-4
+    long a;
+    cin >> a;
+    cout << a%10 << endl;
+    return 0;*/
+
+    /*program_1.2-5
+    long a;
+    cin >> a;
+    cout << a / 1000 * 1000 + ((a / 100) % 10) * 10 + (a / 10) % 10 * 100 + a % 1000 % 100 % 10 << endl;
+    return 0;*/
+}
