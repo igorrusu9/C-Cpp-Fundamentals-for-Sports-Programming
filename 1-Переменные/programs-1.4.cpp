17cb54f649e2948b62fcca650d73143cc12f8595
igorrusu9 <65688712+igorrusu9@users.noreply.github.com>
2021-11-15 22:25:40 -0500

    1-Переменные

---
+++ 1-Переменные/programs-1.4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()	{
+/* programm_1.4-1
+	int a;
+	cin >> a;
+	cout << char(a) << endl;
+	return 0;*/
+
+/* programm_1.4-2
+	char a;
+	cin >> a;
+	cout << int(a) << endl;
+	return 0;*/
+
+/* programm_1.4-3
+	int i;
+	cin >> i;
+	cout << char(i+96) << endl;
+	return 0;*/
+
+/* programm_1.4-4
+	char a;
+	cin >> a;
+	cout << char(a - 32) << endl;
+	return 0;*/
+
+/* programm_1.4-5
+	char a;
+	cin >> a;
+	cout << a << endl;
+	return 0; */
+}
---
+++ 1-Переменные/programs-1.5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()	{
+/* programm_1.5-1*/
+	double a=3.14, b;
+	cin >> b;
+	cout << a << " " << b << endl;
+	return 0;
+
+/* programm_1.5-2
+	char a;
+	cin >> a;
+	cout << int(a) << endl;
+	return 0;*/
+
+/* programm_1.5-3
+	int i;
+	cin >> i;
+	cout << char(i+96) << endl;
+	return 0;*/
+
+/* programm_1.5-4
+	char a;
+	cin >> a;
+	cout << char(a - 32) << endl;
+	return 0;*/
+}
